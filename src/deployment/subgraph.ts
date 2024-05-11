168a1a4d78bfae8bad0b3eaa0507fd09b1cb88f5
Sladuca <sladuca777@gmail.com>
2024-05-11 10:28:24 -0400

    fix setup, attempt to sync with graph node

--- src/deployment/subgraph.ts
+++ src/deployment/subgraph.ts
@@ -1,7 +1,10 @@
 import { sleep, runCommand, ROOT_DIR } from "../utils";
+import { getEnvVars } from "../env";
+import * as fs from "fs";
 import * as compose from "docker-compose";
 
 export const GRAPH_NODE_COMPOSE_CWD = `${ROOT_DIR}/graph-node/docker`;
+
 const GRAPH_NODE_COMPOSE_OPTS: compose.IDockerComposeOptions = {
   cwd: GRAPH_NODE_COMPOSE_CWD,
   commandOptions: [["--force-recreate"], ["--renew-anon-volumes"]],
@@ -14,9 +17,31 @@ export const SUBGRAPH_CWD = `${ROOT_DIR}/subgraph`;
 export async function startSubgraph(): Promise<() => Promise<void>> {
   // start graph node
   console.log("starting graph node...");
-  const res = await compose.upAll(GRAPH_NODE_COMPOSE_OPTS);
-  console.log(res);
+  const { RPC_URL } = getEnvVars();
+  await fs.promises.writeFile(`${GRAPH_NODE_COMPOSE_CWD}/.env`, `RPC_URL=${RPC_URL}`);
+  try {
+    const res = await compose.upAll(GRAPH_NODE_COMPOSE_OPTS);
+    console.log("graph node successfully started", res);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+
+  console.log("waiting for graph node to start...");
   await sleep(20_000);
+
+  // build subgraph
+  try {
+    console.log("building subgraph...");
+    const [stdout, stderr] = await runCommand(`yarn install && yarn codegen && yarn graph build --network mainnet`, SUBGRAPH_CWD);
+    console.log(stdout);
+    if (stderr) {
+      console.error(stderr);
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 
   // deploy subgraph
   try {
@@ -33,6 +58,10 @@ export async function startSubgraph(): Promise<() => Promise<void>> {
     console.error(err);
     throw err;
   }
+
+  // wait for subgraph to sync
+  console.log("waiting for subgraph to sync... this could take a few minutes");
+  await sleep(300_000);
 
   return async () => {
     await compose.down({
